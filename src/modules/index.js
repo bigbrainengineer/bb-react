247978d240db64fc2d721ac2f0f93e26bfdcde5f
Albert Navasardyan <albert.navasardyan@gmail.com>
2018-02-25 12:49:09 +0400

    eslit config added

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,9 +1,12 @@
-import { combineReducers } from 'redux'
-import { routerReducer } from 'react-router-redux'
+import {combineReducers} from 'redux'
+import {routerReducer} from 'react-router-redux'
 import counter from './counter'
 import main from './main'
+import auth from './auth'
+
 export default combineReducers({
-    router: routerReducer,
-    counter,
-    main
+  router: routerReducer,
+  auth,
+  counter,
+  main
 })
