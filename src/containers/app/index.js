14c9dbaa25d728350c42ceba49e0dfb172e2854d
Albert Navasardyan <albert.navasardyan@gmail.com>
2018-02-25 15:55:00 +0400

    jwt functionality added to auth.js

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -1,28 +1,57 @@
 import React from 'react';
 import { Route, Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import Home from '../home';
 import About from '../about';
 import Search from '../main/index';
 
-const App = () => (
-  <div>
-    <header>
-      <nav className="navbar navbar-toggleable-md navbar-light bg-faded">
-        <div className="collapse navbar-collapse show">
-          <div className="navbar-nav">
-            <Link to="/" className="nav-item nav-link active">Home <span className="sr-only">(current)</span></Link>
-            <Link to="/about-us" className="nav-item nav-link">About</Link>
+const App = (props) => {
+  const navClass = 'nav-item nav-link';
+  return (
+    <div>
+      <header>
+        <nav className="navbar navbar-toggleable-md navbar-light bg-faded">
+          <div className="collapse navbar-collapse show">
+            <div className="navbar-nav">
+              <Link
+                to="/"
+                className={`${navClass} ${props.pathname === '/' ? 'active' : ''}`}
+              >
+                Home
+              </Link>
+              <Link
+                to="/about-us"
+                className={`${navClass} ${props.pathname === '/about-us' ? 'active' : ''}`}
+              >
+                About
+              </Link>
+              <div className="nav-item nav-link">
+                { props.isAuthenticated ? 'Logged in' : 'Logged out' }
+              </div>
+            </div>
           </div>
-        </div>
-      </nav>
-    </header>
+        </nav>
+      </header>
 
-    <main className="container">
-      <Route exact path="/" component={Home} />
-      <Route exact path="/about-us" component={About} />
-      <Route exact path="/search" component={Search} />
-    </main>
-  </div>
-);
+      <main className="container">
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about-us" component={About} />
+        <Route exact path="/search" component={Search} />
+        {/* <Route exact path="/login" component={Search}/> */}
+      </main>
+    </div>
+  );
+};
 
-export default App;
+App.propTypes = {
+  pathname: PropTypes.string.isRequired,
+  isAuthenticated: PropTypes.bool.isRequired
+};
+
+const mapStateToProps = state => ({
+  pathname: state.router.location.pathname,
+  isAuthenticated: state.auth.isAuthenticated,
+});
+
+export default connect(mapStateToProps)(App);
